96f55b80a18143919dfd266243d7ad5054030bff
Itamar Shoham <itsho.itsho@gmail.com>
2018-08-19 09:46:30 +0300

    * Infra - Use .Net 4.5 and VS2017
    * Logic - read & parse files on a different thread
    * Logic - support StaticResource with following multiple spaces (Regex)
    * Logic - support DynamicResource (Regex)
    * UI - Added Progress-bar
    * UI - first grid - group items by file name
    * UI - added second grid to display the usages of selected item in first grid
    * UI - double click on any of the grids will open the file location, and COPY the resource key to the clipboard

--- ResourceCounter/ResourceCounter/MainWindow.xaml.cs
+++ ResourceCounter/ResourceCounter/MainWindow.xaml.cs
@@ -1,12 +1,80 @@
-﻿using System.Windows;
+﻿using ResourceCounter.Models;
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using ResourceCounter.ViewModels;
 
-namespace ResourceCounter {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window {
-        public MainWindow() {
+namespace ResourceCounter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
             InitializeComponent();
+        }
+
+        private void masterGrid_OnDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var row = sender as DataGridRow;
+                if (row != null)
+                {
+                    var resourceDeclaration = row.DataContext as DataResource;
+                    
+
+                    if (resourceDeclaration == null || !File.Exists(resourceDeclaration.DefinedInXamlFile.Path))
+                    {
+                        return;
+                    }
+
+                    // combine the arguments together
+                    // it doesn't matter if there is a space after ','
+                    string argument = $@"/select, ""{resourceDeclaration.DefinedInXamlFile.Path}""";
+
+                    System.Diagnostics.Process.Start("explorer.exe", argument);
+
+                    Clipboard.SetText(resourceDeclaration.Key);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while opening declaration file:" + ex.Message);
+            }
+           
+        }
+
+        private void detailGrid_OnDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var vm = DataContext as MainViewModel;
+                var row = sender as DataGridRow;
+
+                if (row != null && vm != null)
+                {
+                    var usageXamlFile = row.DataContext as XamlFile;
+
+                    if (usageXamlFile == null || !File.Exists(usageXamlFile.Path))
+                    {
+                        return;
+                    }
+
+                    // combine the arguments together
+                    // it doesn't matter if there is a space after ','
+                    string argument = $@"/select, ""{usageXamlFile.Path}""";
+
+                    System.Diagnostics.Process.Start("explorer.exe", argument);
+
+                    Clipboard.SetText(vm.SelectedResource.Key);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error while opening usage file:" + ex.Message);
+            }
         }
     }
 }
--- ResourceCounter/ResourceCounter/ViewModels/MainViewModel.cs
+++ ResourceCounter/ResourceCounter/ViewModels/MainViewModel.cs
@@ -1,110 +1,255 @@
-﻿using System.Collections.ObjectModel;
-using System.IO;
+﻿using ResourceCounter.helpers;
 using ResourceCounter.Models;
+using System;
 using System.Collections.Generic;
-using System.Windows.Input;
-using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
-using ResourceCounter.helpers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Threading;
 
-namespace ResourceCounter.ViewModels {
-    public class MainViewModel : ObservableObject {
-        private string rootPath;
-        private List<XamlFile> xamlFiles;
-        public ObservableCollection<DataResource> ApplicationResources { get; set; }
+namespace ResourceCounter.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        #region Data members
 
-        public string RootPath {
-            get { return rootPath; }
-            set {
-                if (rootPath != value) {
-                    rootPath = value;
-                    OnPropertyChanged(() => RootPath);
-                    var s = new Settings { RootPath = value };
-                    Serializer.SaveSettings(s);
+        private string _rootPath;
+        private List<XamlFile> _xamlFiles;
+        private bool _isBusy;
+        private readonly Dispatcher _uiDispatcher;
+        private int _analyzingProgressValue;
+        private int _analyzingProgressMax;
+        private DataResource _selectedResource;
+
+        #endregion Data members
+
+        #region Properties
+
+        public ObservableCollection<DataResource> ApplicationResourcesFlat { get; private set; }
+        public ListCollectionView ApplicationResourcesGrouped { get; private set; }
+
+        public DataResource SelectedResource
+        {
+            get => _selectedResource;
+            set
+            {
+                if (_selectedResource != value)
+                {
+                    _selectedResource = value;
+                    OnPropertyChanged(() => SelectedResource);
                 }
             }
         }
 
-        public ICommand AnalyseCommand { get; set; }
-
-        public MainViewModel() {
-            AnalyseCommand = new DelegateCommand<object>(o => {
-                Analyse();
-            }, o => {
-                return true;
-            });
-
-            ApplicationResources = new ObservableCollection<DataResource>();
-
-            RootPath = Serializer.LoadSettings<Settings>().RootPath;
-        }
-
-        bool IsUsed(string staticResource) {
-            foreach (var xamlFile in xamlFiles) {
-                if (xamlFile.XAML.IndexOf("StaticResource " + staticResource) > -1)
-                    return true;
+        public string RootPath
+        {
+            get { return _rootPath; }
+            set
+            {
+                if (_rootPath != value)
+                {
+                    _rootPath = value;
+                    OnPropertyChanged(() => RootPath);
+                }
             }
-            return false;
         }
 
-        private void DetermineStyles(XamlFile xamlFile) {
+        public ICommand AnalyzeCommand { get; set; }
+
+        public int AnalyzingProgressMax
+        {
+            get => _analyzingProgressMax;
+            set
+            {
+                if (_analyzingProgressMax != value)
+                {
+                    _analyzingProgressMax = value;
+                    OnPropertyChanged(() => AnalyzingProgressMax);
+                }
+            }
+        }
+
+        public int AnalyzingProgressValue
+        {
+            get => _analyzingProgressValue;
+            set
+            {
+                if (_analyzingProgressValue != value)
+                {
+                    _analyzingProgressValue = value;
+                    OnPropertyChanged(() => AnalyzingProgressValue);
+                }
+            }
+        }
+
+        public bool IsBusy
+        {
+            get => _isBusy;
+            set
+            {
+                if (_isBusy != value)
+                {
+                    _isBusy = value;
+                    OnPropertyChanged(() => IsBusy);
+                }
+            }
+        }
+
+        #endregion Properties
+
+        public MainViewModel()
+        {
+            AnalyzeCommand = new DelegateCommand<object>(o => { Task.Factory.StartNew(Analyze); }, o => !IsBusy);
+
+            ApplicationResourcesFlat = new ObservableCollection<DataResource>();
+            ApplicationResourcesGrouped = new ListCollectionView(ApplicationResourcesFlat);
+            ApplicationResourcesGrouped.GroupDescriptions.Add(new PropertyGroupDescription("DefinedInXamlFile.FileName"));
+
+            _uiDispatcher = Dispatcher.CurrentDispatcher;
+
+            // if NOT designMode
+            if (!(bool) (DesignerProperties.IsInDesignModeProperty.GetMetadata(typeof(DependencyObject)).DefaultValue))
+            {
+                RootPath = Serializer.LoadSettings<Settings>().RootPath;
+                SaveSettings();
+            }
+        }
+
+     /*   private bool IsUsed(string staticResource)
+        {
+            foreach (var xamlFile in _xamlFiles)
+            {
+                if (xamlFile.XAML.IndexOf("StaticResource " + staticResource) > -1)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }*/
+
+        private void DetermineStyles(XamlFile xamlFile)
+        {
             int pos = xamlFile.XAML.IndexOf("x:Key=", 0);
 
-            while (pos > -1) {
+            while (pos > -1)
+            {
                 int end = xamlFile.XAML.IndexOf("\"", pos + 7);
                 var key = xamlFile.XAML.Substring(pos + 7, end - pos - 7);
-                ApplicationResources.Add(new DataResource { DefinedInXamlFile = xamlFile, Key = key, UsedInXamlFiles = new List<XamlFile>() });
+
+                _uiDispatcher.Invoke(new Action(() =>
+                {
+                    ApplicationResourcesFlat.Add(new DataResource
+                    {
+                        DefinedInXamlFile = xamlFile,
+                        Key = key,
+                        UsedInXamlFiles = new List<XamlFile>()
+                    });
+                }));
+
                 pos = xamlFile.XAML.IndexOf("x:Key=", end);
             }
         }
 
-        private void Analyse() {
-            ApplicationResources.Clear();
+        private void SaveSettings()
+        {
+            var s = new Settings { RootPath = RootPath };
+            Serializer.SaveSettings(s);
+        }
 
-            xamlFiles = new List<XamlFile>();
-            LoadFiles(rootPath);
+        private void Analyze()
+        {
+            try
+            {
+                AnalyzingProgressValue = 0;
+                AnalyzingProgressMax = 1000000;
+                IsBusy = true;
+                _uiDispatcher.Invoke(new Action(() => { ApplicationResourcesFlat.Clear(); }));
 
-            foreach (var xamlFile in xamlFiles) {
-                DetermineStyles(xamlFile);
-            }
+                _xamlFiles = new List<XamlFile>();
+                LoadFiles(_rootPath);
+                AnalyzingProgressMax = _xamlFiles.Count * 2;
 
-            foreach (var rec in ApplicationResources) {
-                foreach (var xamlFile in xamlFiles) {
-                    xamlFile.Count = new Regex("StaticResource " + rec.Key).Matches(xamlFile.XAML).Count;
-                    if (xamlFile.Count > 0) {
-                        rec.UsedInXamlFiles.Add(xamlFile);
+                foreach (var xamlFile in _xamlFiles)
+                {
+                    AnalyzingProgressValue++;
+                    DetermineStyles(xamlFile);
+                }
+
+                AnalyzingProgressMax = _xamlFiles.Count + (_xamlFiles.Count * ApplicationResourcesFlat.Count);
+
+                foreach (var rec in ApplicationResourcesFlat)
+                {
+                    foreach (var xamlFile in _xamlFiles)
+                    {
+                        AnalyzingProgressValue++;
+
+                        // find StaticResource with OR without specifying "ResourceKey=" (with any number of spaces between)
+                        var regexToUse = $@"{{StaticResource[ ]{{1,}}(ResourceKey=){{0,1}}{rec.Key}" +
+                                         // find DynamicResource with OR without specifying "ResourceKey=" (with any number of spaces between)
+                                         $@"|{{DynamicResource[ ]{{1,}}(ResourceKey=){{0,1}}{rec.Key}";
+
+                        xamlFile.Count = new Regex(regexToUse).Matches(xamlFile.XAML).Count;
+                        if (xamlFile.Count > 0)
+                        {
+                            rec.UsedInXamlFiles.Add(xamlFile);
+                            rec.RaisePropertyChangedTotalOccurrences();
+                        }
                     }
                 }
+
+                OnPropertyChanged(() => UnusedCount);
             }
-
-            OnPropertyChanged(() => UnusedCount);
-
+            catch (Exception e)
+            {
+            }
+            finally
+            {
+                IsBusy = false;
+            }
         }
 
-        public int UnusedCount {
-            get {
-                return ApplicationResources.Count(x => x.UsedInXamlFiles.Count == 0);
-            }
+        public int UnusedCount
+        {
+            get { return ApplicationResourcesFlat.Count(x => x.UsedInXamlFiles.Count == 0); }
         }
 
-        private void LoadFiles(string path) {
+        private void LoadFiles(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("Unable to load folder");
+                return;
+            }
+
             var di = new DirectoryInfo(path);
             var items = di.GetFiles("*.xaml");
-            foreach (var item in items) {
-                xamlFiles.Add(new XamlFile {
+            foreach (var item in items)
+            {
+                _xamlFiles.Add(new XamlFile
+                {
                     Path = item.FullName,
                     XAML = LoadXAML(item.FullName),
                 });
             }
 
             var folders = di.GetDirectories();
-            foreach (var folder in folders) {
+            foreach (var folder in folders)
+            {
                 LoadFiles(folder.FullName);
             }
         }
 
-        string LoadXAML(string path) {
-            using (var reader = new StreamReader(path)) {
+        private string LoadXAML(string path)
+        {
+            using (var reader = new StreamReader(path))
+            {
                 var text = reader.ReadToEnd();
                 reader.Close();
                 return text;
